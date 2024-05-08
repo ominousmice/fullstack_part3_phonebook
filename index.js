509c37424f2fd35463c4b3fcef81b382f16e332d
ominousmice <nicoleynicoleal@gmail.com>
2024-05-08 17:54:47 -0300

    ex 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -55,6 +57,14 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(p => p.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = Math.floor(Math.random() * 100000)
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
