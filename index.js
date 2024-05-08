99a8717b6dad6343c7d9cb6ad9f154185fe2caf4
ominousmice <nicoleynicoleal@gmail.com>
2024-05-08 17:25:26 -0300

    ex 3.2

--- index.js
+++ index.js
@@ -24,12 +24,19 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${Date()}</p>
+        </div>
+        `
+    )
 })
 
 const PORT = 3001
