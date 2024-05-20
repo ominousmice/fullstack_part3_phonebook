4195a7e632627e545818841bc2dfcea6c59d4ef9
ominousmice <nicoleynicoleal@gmail.com>
2024-05-20 18:27:00 -0300

    ex 3.19

--- index.js
+++ index.js
@@ -80,20 +80,8 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if(!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
 
     const person = new Person({
         name: body.name,
@@ -103,18 +91,17 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    console.log(body)
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(updatedPerson => {
         response.json(updatedPerson)
         })
@@ -133,7 +120,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
     next(error)
 }
--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -32,6 +32,10 @@ const App = () => {
           setSuccessMessage(`${newPerson.name} was added`)
           setTimeout(() => {setSuccessMessage(null)}, 5000)
         })
+        .catch(error => {
+          setErrorMessage(error.response.data.error)
+          setTimeout(() => {setErrorMessage(null)}, 5000)
+        })
 
     } else {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
